b7dad21d0be3e39c273aed73accc69d294eafe8f
Timothy Paek <tax2farm@gmail.com>
2023-06-27 15:28:45 -0500

    balance sheet and incomplete projects

---
+++ Surgeon Slideshow/code.js
@@ -0,0 +1,23 @@
+var slide = 1;
+
+showSlide(slide);
+
+function nextSlide(n)
+{
+    showSlide(slide += n);
+}
+
+function showSlide(n)
+{
+    var i;
+    var slides = document.getElementsByClassName("Surgeon");
+    if(n > slides.length) {slide = 1}
+    if(n < 1) {slide = slides.length};
+
+    for(i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+    slides[slide-1].style.display = "block";
+
+}
